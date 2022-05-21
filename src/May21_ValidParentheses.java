f42a4dcb19f9cbdbe01048fc349df74421d02093
Radheshyam Soni <radheshyambsoni@gmail.com>
2022-05-21 12:32:38 +0530

    added May 21 files

---
+++ src/May21_ValidParentheses.java
@@ -0,0 +1,66 @@
+public class May21_ValidParentheses{
+    private char arr[];
+    private int top;
+    private int capacity;
+
+    public void create(int size){
+        arr=new char[size];
+        capacity=size;
+        top=-1;
+    }
+
+    public void push(char x){
+        if(isFull()){
+            System.out.println("Stack OverFlow");
+            System.exit(1);
+        }
+
+        arr[++top]=x;
+    }
+
+    public char pop(){
+        if(isEmpty()){
+            System.out.println("STACK EMPTY");
+            System.exit(1);
+        }
+
+        return arr[top--];
+    }
+
+    public int getSize(){return top+1;}
+
+    public Boolean isEmpty(){return top==-1;}
+
+    public Boolean isFull(){return top==capacity-1;}
+
+    public void printStack(){
+        for(int i=0; i <= top; i++){
+            System.out.print(arr[i]+", ");
+        }
+    }
+
+    
+    public boolean isValid(String s){
+        this.create(s.length());
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(' || s.charAt(i)=='[' || s.charAt(i)=='{'){
+                if(s.charAt(i)=='(' || s.charAt(i)=='[' || s.charAt(i)=='{'){
+                    this.push(s.charAt(i));
+                }
+            }else if(s.charAt(i)==')' || s.charAt(i)==']' || s.charAt(i)=='}'){
+                if(!this.isEmpty() && ((s.charAt(i)==')' && this.arr[top]=='(') || (s.charAt(i)==']' && this.arr[top]=='[') || (s.charAt(i)=='}' && this.arr[top]=='{'))){
+                    this.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+
+        return isEmpty();
+    }
+    public static void main(String[] args) {
+        String s="]";
+        May21_ValidParentheses t=new May21_ValidParentheses();
+        System.out.println(t.isValid(s));
+    }
+}
