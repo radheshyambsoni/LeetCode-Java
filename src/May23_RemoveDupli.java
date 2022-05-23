0292cb369916245ddf88ce62bec2f4e5c2454e1b
Radheshyam Soni <radheshyambsoni@gmail.com>
2022-05-23 23:09:26 +0530

    May 23 - 1 que solved

---
+++ src/May23_RemoveDupli.java
@@ -0,0 +1,28 @@
+// Remove Duplicates from Sorted Array
+public class May23_RemoveDupli {
+    public static int removeDuplicates(int[] nums) {
+        int idx=0;
+        int temp=-101;
+        for(int i=0;i<nums.length;i++){
+            if(temp!=nums[i]){
+                temp=nums[i];
+                nums[idx]=nums[i];
+                idx++;
+            }
+        }
+
+        return idx;
+    }
+    public static void main(String[] args) {
+        int[] nums ={0,0,1,1,1,2,2,3,3,4}; // Input array
+        int[] expectedNums ={0,1,2,3,4}; // The expected answer with correct length
+        
+        int k = removeDuplicates(nums); // Calls your implementation
+        
+        assert k == expectedNums.length;
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i]+" ");
+            assert nums[i] == expectedNums[i];
+        }
+    }
+}
