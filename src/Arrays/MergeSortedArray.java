fff9a9f998fb321079a430b3e5d21e13fa45b301
Radheshyam Soni <radheshyambsoni@gmail.com>
2022-07-07 21:34:34 +0530

    adding 7th July 2022 leetcode submissions

---
+++ src/Arrays/MergeSortedArray.java
@@ -0,0 +1,29 @@
+package Arrays;
+import java.util.Arrays;
+
+// 88. Merge Sorted Array
+// https://leetcode.com/problems/merge-sorted-array/
+// Related Topics - Arrays, Two pointers, Sorting
+
+public class MergeSortedArray {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        m--;n--;
+        while(m>=0 && n>=0){
+            if(nums1[m]>nums2[n]){
+                nums1[m+n+1]=nums1[m--];
+            }else{
+                nums1[m+n+1]=nums2[n--];
+            }
+        }while(m>=0){
+            nums1[m+n+1]=nums1[m--];
+        }while(n>=0){
+            nums1[m+n+1]=nums2[n--];
+        }
+    }
+    public static void main(String[] args) {
+        int nums1[]={7,8,9,0,0,0};
+        int nums2[]={2,4,5};
+        merge(nums1,3, nums2,3);
+        System.out.println(Arrays.toString(nums1));
+    }
+}
--- src/temp.java
+++ src/temp.java
@@ -1,22 +1,42 @@
-import java.util.List;
-
-public class temp {
-    public static List<Integer> addToArrayForm(int[] num, int k) {
-        long number=0;
-        for(int i=0;i<num.length;i++){
-            number=number*10+num[i];
+import java.util.Arrays;
+class Solution{
+    public static int[] sortedSquares(int[] nums) {
+        int i=0,k=nums.length-1;
+        while(k>=0){
+            if(k==0){nums[k]*=nums[k--];}
+            else if(i==k){i=0;}
+            else if(Math.abs(nums[i])==Math.abs(nums[k])){
+                i++;
+            }else if(Math.abs(nums[i])>Math.abs(nums[k])){
+                int temp=nums[i];
+                nums[i]=nums[k];
+                nums[k--]=temp*temp;
+            }else{
+                i++;
+            }
         }
-        number+=k;
-        System.out.println(number);
-        System.out.println(number%10);
-        int ans[]=new int[(int)Math.log(number)+1];
-        for(int i=ans.length;i>=0;i--){
-            // 
-        }
 
-
+        return nums;
     }
     public static void main(String[] args) {
-        System.out.println(addToArrayForm(new int[]{9,9,9,9,9,9,9,9,9,9}, 1));
+        int nums[]={-10000,-9999,-7,-5,5,6,10000};
+        // -10000 -9999 -7 -5 0 0 10000
+        // k=6,i=0 // -10000 -9999 -7 -5 5 6
+        // k=5,i=0 // 6 -9999 -7 -5 5
+        // k=4,i=0 // 
+        // k=4,i=1 // 0 0 -7 -5
+        // k=3,i=1 // 0 0 -7 -5
+        // k=3,i=2 // 0 0 -5
+        // k=2,i=2 // 
+        sortedSquares(nums);
+        System.out.println(Arrays.toString(nums));
     }
 }
+
+// -5 -3 -2 9
+// k=3,i=0
+// -5 -3 -2 81
+// k=2,i=0
+// -2 -3 25 81
+// k=1 i=0
+// 
