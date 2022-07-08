417f6382b3073d6fbe3a1c5504a6f413647aa135
Radheshyam Soni <radheshyambsoni@gmail.com>
2022-07-08 19:44:03 +0530

    Added July 8, 2022 leetcode submission

---
+++ src/Arrays/RemoveElement.java
@@ -0,0 +1,32 @@
+// Took too manys submissions - try to resolve
+package Arrays;
+// 27. Remove Element
+// https://leetcode.com/problems/remove-element/
+// Related Topics - Arrays Two Pointers
+
+public class RemoveElement{
+    public static int removeElement(int[] nums, int val) {
+        if(val>50){return (nums.length-1);}
+        if(nums.length==0){
+            return 0;
+        }
+        int i=0,j=nums.length-1;
+        while(i<=j){
+            while(j>=0 && nums[j]==val){j--;}
+            if(i>j){break;}
+            if(nums[i]==val){
+                nums[i]=nums[j];
+                nums[j]=val;
+            }
+            i++;
+        }
+        return j+1;
+    }
+    public static void main(String[] args) {
+        int nums[]={3};
+        int k=removeElement(nums,3);
+        for(int i=0;i<k;i++){
+            System.out.print(nums[i]+" ");
+        }
+    }
+}
