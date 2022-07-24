e68e1a6ea54f3819374060cb570a391c4f19c887
Radheshyam Soni <radheshyambsoni@gmail.com>
2022-07-24 21:19:25 +0530

    "Added July 24, 2022 submission(s)"

---
+++ src/Arrays/Search2DMatrixII.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+// Best solution copied from submissions
+// 240. Search a 2D Matrix II - Medium
+// https://leetcode.com/problems/search-a-2d-matrix-ii/
+// Related Topics - Array, Binary Search, Divide n Conquer, Matrix
+
+public class Search2DMatrixII{
+    public boolean searchMatrix(int[][] matrix, int target) {        
+        int i=matrix.length-1;
+        int j=0;
+        while(i>=0 && j<matrix[0].length){
+            if(matrix[i][j]==target){
+                return true;
+            }else if(matrix[i][j]<target){
+                j++;
+            }else{
+                i--;
+            }
+        }
+        return false;
+    }
+}
