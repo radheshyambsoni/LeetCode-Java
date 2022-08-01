5c19a4c55393c022f09b3768b23c5dc03be66310
Radheshyam Soni <radheshyambsoni@gmail.com>
2022-08-01 20:49:24 +0530

    Added July 31, 2022 submission(s)

---
+++ src/BinarySearch/BinarySearch.java
@@ -0,0 +1,22 @@
+// 704. Binary Search - Easy
+// https://leetcode.com/problems/binary-search/
+// Related Topics - Array, Binary Search
+
+public class BinarySearch{
+    public int search(int[] nums, int target) {
+        int start=0,end=nums.length-1;
+        int mid=start+(end-start)/2;
+        while(start<=end){
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]>target){
+                end=mid-1;
+                mid=start+(end-start)/2;                
+            }else{
+                start=mid+1;
+                mid=start+(end-start)/2;
+            }
+        }
+        return -1;
+    }
+}
