57c7a5a5dd0dca004e777fcfca08e0571dc7c4af
Radheshyam Soni <91137834+radheshyambsoni@users.noreply.github.com>
2022-08-30 23:34:48 +0530

    Added August 30, 2022 submission(s)

---
+++ src/NumbersNMath/PowerOfTwo.java
@@ -0,0 +1,17 @@
+package NumbersNMath;
+
+// 231. Power of Two - Easy
+// https://leetcode.com/problems/power-of-two/
+// Related Topics - Math, Bit Manipulation, Recursion
+// Runtime: 1 ms, faster than 100.00% of Java online submissions for Power of Two.
+// Memory Usage: 39.8 MB, less than 87.28% of Java online submissions for Power of Two.
+
+public class test{
+    public static boolean minimumTotal(int n) {
+        if(n<=0){
+            return false;
+        }
+        double check=Math.log10(n)/Math.log10(2);
+        return check-(int)check==0;
+    }
+}
