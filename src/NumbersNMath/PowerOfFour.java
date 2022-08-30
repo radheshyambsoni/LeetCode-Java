4974171b3be50575406bd0347c9b0e5ebe0872d3
Radheshyam Soni <91137834+radheshyambsoni@users.noreply.github.com>
2022-08-30 23:47:02 +0530

    Added August 30, 2022 submission(s)

---
+++ src/NumbersNMath/PowerOfFour.java
@@ -0,0 +1,12 @@
+package NumbersNMath;
+
+// 342. Power of Four
+// https://leetcode.com/problems/power-of-two/
+// Related Topics - Math, Bit Manipulation, Recursion
+
+public class PowerOfFour{
+    public static boolean minimumTotal(int n) {
+        double check=Math.log10(n)/Math.log10(4.0);
+        return check==(int)check;
+    }
+}
