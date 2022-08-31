304e0da1749bd814b768292c8253d27d9e08e743
Radheshyam Soni <91137834+radheshyambsoni@users.noreply.github.com>
2022-08-31 18:37:39 +0530

    Added August 31, 2022 submission(s)

--- src/BinarySearch/SearchInRotatedSortedArray.java
+++ src/BinarySearch/SearchInRotatedSortedArray.java
@@ -43,10 +43,10 @@ public class SearchInRotatedSortedArray {
         int start=0,end=arr.length-1;
         while(start<=end){
             int mid=start+(end-start)/2;
-            if(mid<arr.length-1 && arr[mid]>arr[mid+1]){
+            if(mid<end && arr[mid]>arr[mid+1]){
                 return mid;
             }
-            if(mid>0 && arr[mid]<arr[mid-1]){
+            if(mid>start && arr[mid]<arr[mid-1]){
                 return mid-1;
             }
             if(arr[mid]<arr[start]){
