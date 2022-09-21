7d6aac754258c0d2fbdcd474b1223dd8d8af946c
Radheshyam Soni <91137834+radheshyambsoni@users.noreply.github.com>
2022-09-21 19:27:30 +0530

    Update CellsWithOddValuesInMatrix.java

--- src/Arrays/CellsWithOddValuesInMatrix.java
+++ src/Arrays/CellsWithOddValuesInMatrix.java
@@ -1,34 +1,30 @@
 package Arrays;
 
-// 1252. Cells with Odd Values in a Matrix
+// Optimised version came from submissions section
 // https://leetcode.com/problems/cells-with-odd-values-in-a-matrix/submissions/
-// Related Topics - Array, Math, Simulation
+// Runtime: 2 ms, faster than 71.71% of Java online submissions for Cells with Odd Values in a Matrix.
+// Memory Usage: 42.7 MB, less than 19.40% of Java online submissions for Cells with Odd Values in a Matrix.
 
 public class CellsWithOddValuesInMatrix {
-    public static int oddCells(int m, int n, int[][] indices) {
-        int count=0;
-        int[][] ans=new int[m][n];
-        for(int i=0;i<indices.length;i++){
-            int j=indices[i][0];
-            for(int k=0;k<n;k++){
-                ans[j][k]++;
-            }
-            j=indices[i][1];
-            for(int k=0;k<m;k++){
-                ans[k][j]++;
-            }
+    public int oddCells(int m, int n, int[][] indices) {
+        // the value at any index of rows and cols arrays 
+        // here represents the number of times the row/column is incremented 
+        int rows[]=new int[m];
+        int cols[]=new int[n];
+        for(int idx[]: indices){
+            rows[idx[0]]++;
+            cols[idx[1]]++;
         }
+        int count=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(ans[i][j]%2!=0){
+                // here the sum 'rows[i]+cols[j]' represents the number of times
+                // the actual index in the expected array would have been incremented
+                if((rows[i]+cols[j])%2==1){
                     count++;
                 }
             }
         }
-        
         return count;
-    }
-    public static void main(String[] args) {
-        System.out.println(oddCells(2, 3, new int[][]{{0,1},{1,1}}));
     }
 }
