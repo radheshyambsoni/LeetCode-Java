732c454721234e47549cd7c4754d22e23b51f4c6
Radheshyam Soni <radheshyambsoni@gmail.com>
2022-09-21 21:46:04 +0530

    Added September 21, 2022 submission(s)

---
+++ src/Arrays/FindNumsWithEvenNumOfDigits.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+// https://leetcode.com/problems/find-numbers-with-even-number-of-digits/ - Easy
+
+public class FindNumsWithEvenNumOfDigits {
+    public int findNumbers(int[] nums) {
+        // clever trick is to tackle the range (based on constraints i.e. 1<=nums[i]<=10^5) where the even number of digits number belong
+        int count=0;
+        for(int i:nums){
+            if(i>9 && i<100 || i>999 && i<10000 || i==100000){
+                count++;
+            }
+        }
+        return count;
+    }
+}
