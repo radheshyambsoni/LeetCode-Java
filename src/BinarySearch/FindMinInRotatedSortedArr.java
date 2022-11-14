57c4aa4eced9242239555886e41326c7908a29e5
Radheshyam B. Soni <radheshyambsoni@gmail.com>
2022-11-14 13:09:26 +0530

    Added November 14, 2022 submission(s)

---
+++ src/BinarySearch/FindMinInRotatedSortedArr.java
@@ -0,0 +1,48 @@
+package BinarySearch;
+
+public class FindMinInRotatedSortedArr {
+    // Runtime 1 ms Beats 60.13%
+    // Memory 43 MB Beats 10.1%
+    // static int findPivot(int[] arr ){
+    //     int start=0,end=arr.length-1;
+    //     while(start<=end){
+    //         int mid=start+(end-start)/2;
+    //         if(mid<end && arr[mid]>arr[mid+1]){
+    //             return mid;
+    //         }
+    //         if(mid>start && arr[mid]<arr[mid-1]){
+    //             return mid-1;
+    //         }
+    //         if(arr[mid]<arr[start]){
+    //             end=mid-1;
+    //         }else{
+    //             start=mid+1;
+    //         }
+    //     }
+    //     return -1;
+    // }
+    // public static int findMin(int[] nums) {
+    //     return nums[findPivot(nums)+1];
+    // }
+
+    // Runtime 0 ms Beats 100%
+    // Memory 42.9 MB Beats 19.87%
+    public static int findMin(int[] nums) {
+        if(nums[0]<nums[nums.length-1]){
+            return nums[0];
+        }
+        int s=0,e=nums.length-1;        
+        while(s<e){
+            int m=s+(e-s)/2;
+            if(nums[m]<nums[0]){
+                e=m;
+            }else{
+                s=m+1;
+            }
+        }
+        return nums[e];
+    }
+    public static void main(String[] args) {
+        System.out.println(findMin(new int[]{3,4,5,1}));
+    }
+}
