6f37429cdf5cb8529c2e77cb88c61aaa157e00d2
Radheshyam B. Soni <radheshyambsoni@gmail.com>
2022-12-11 22:53:40 +0530

    Added December 11, 2022 submission(s)

---
+++ src/NumbersNMath/SignOfProductOfArr.java
@@ -0,0 +1,20 @@
+package NumbersNMath;
+
+// https://leetcode.com/problems/sign-of-the-product-of-an-array/ - Easy
+// Runtime 1 ms Beats 79.56%
+// Memory 44.1 MB Beats 44.88%
+
+public class SignOfProductOfArr{
+    public int arraySign(int[] nums) {
+        int prod=1;
+        for(int i:nums){
+            if(i==0){
+                return 0;
+            }
+            if(i<0){
+                prod*=-1;
+            }
+        }
+        return prod;
+    }
+}
