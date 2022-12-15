b58e0dcba85133c0d92b966cc40cf0e765cb52c5
Radheshyam B. Soni <radheshyambsoni@gmail.com>
2022-12-15 08:35:17 +0530

    Added December 15, 2022 submission(s)

---
+++ src/LinkedList/ConvertBinNumInLL2Int.java
@@ -0,0 +1,17 @@
+package LinkedList;
+
+// https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/ - Easy
+// Runtime 0 ms Beats 100%
+// Memory 39.6 MB Beats 94.53%
+// December 15, 2022
+
+public class ConvertBinNumInLL2Int {
+    public int getDecimalValue(ListNode head) {
+        int ans=0;
+        while(head!=null){
+            ans=ans*2+head.val;
+            head=head.next;
+        }
+        return ans;
+    }
+}
