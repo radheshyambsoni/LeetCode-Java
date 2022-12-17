6368cf04db55b4cc7d34c5e7503ab9e5aafe35b4
Radheshyam B. Soni <radheshyambsoni@gmail.com>
2022-12-17 11:03:07 +0530

    Added December 17, 2022 submission(s)

---
+++ src/Stack/EvalRPN.java
@@ -0,0 +1,38 @@
+package Stack;
+
+import java.util.Stack;
+
+// https://leetcode.com/problems/evaluate-reverse-polish-notation/ - Medium
+// Runtime 6 ms Beats 89.26%
+// Memory 42 MB Beats 91.46%
+// December 17, 2022 - Daily LeetCode Challenge
+
+public class EvalRPN{
+    public static int evalRPN(String[] tokens){
+        Stack<Integer> s=new Stack<>();
+        int val1,val2;
+        for(int i=0;i<tokens.length;i++){
+            if(tokens[i].equals("+") || tokens[i].equals("-") || tokens[i].equals("*") || tokens[i].equals("/")){
+                val1=s.pop();
+                val2=s.pop();
+                if(tokens[i].equals("+")){
+                    val2+=val1;
+                }else if(tokens[i].equals("-")){
+                    val2-=val1;
+                }else if(tokens[i].equals("*")){
+                    val2*=val1;
+                }else{
+                    val2/=val1;
+                }
+                s.push(val2);
+            }else{
+                s.push(Integer.parseInt(tokens[i]));
+            }
+        }
+        return s.pop();
+    }
+    public static void main(String[] args) {
+        String[] s={"10","6","9","3","+","-11","*","/","*","17","+","5","+"};
+        System.out.println(evalRPN(s));
+    }
+}
