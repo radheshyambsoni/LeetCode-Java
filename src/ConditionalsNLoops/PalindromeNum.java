baa7943a142fc4b4b961884aafea31d7f0e6e8e3
Radheshyam B. Soni <radheshyambsoni@gmail.com>
2023-01-11 21:28:55 +0530

    Added January 11, 2023 submission(s)

--- src/ConditionalsNLoops/PalindromeNum.java
+++ src/ConditionalsNLoops/PalindromeNum.java
@@ -1,37 +1,38 @@
 package ConditionalsNLoops;
-import java.util.Scanner;
 
-// https://leetcode.com/problems/palindrome-number/
-// 9. Palindrome Number
+// https://leetcode.com/problems/palindrome-number/ - Easy
+// Runtime 8 ms Beats 99.83%
+// Memory 41.5 MB Beats 92.59%
+// January 10, 2023
 
 public class PalindromeNum {
-    // Given an integer x, return true if x is palindrome integer.
-    // An integer is a palindrome when it reads the same backward as forward.
-    // For example, 121 is a palindrome while 123 is not.
-    // Follow up: Could you solve it without converting the integer to a string?
-    
-    public static boolean isPalindrome(int x) {
-        if(x<0){return false;}
-        
-        int size=(int)Math.log10(x)+1;
-        if(size==1){return true;}
-        int ans=0;
-        
-        for(int i=0;i<size/2;i++){
-            ans=ans*10+x%10;
+    public boolean isPalindrome(int x) {
+        if(x<0 || (x%10==0 && x!=0)){return false;}
+
+        int n=0;
+        while(x>n){
+            n=n*10+x%10;
             x/=10;
         }
-        
-        if(size%2==1){x/=10;}
-        if(x==ans){return true;}
-        return false;
+
+        if(x==n){return true;}
+        return x==(n/10);
     }
     
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        sc.close();
-
-        System.out.println(isPalindrome(n));
-    }
+    // public static boolean isPalindrome(int x) {
+    //     if(x<0){return false;}
+        
+    //     int size=(int)Math.log10(x)+1;
+    //     if(size==1){return true;}
+    //     int ans=0;
+        
+    //     for(int i=0;i<size/2;i++){
+    //         ans=ans*10+x%10;
+    //         x/=10;
+    //     }
+        
+    //     if(size%2==1){x/=10;}
+    //     if(x==ans){return true;}
+    //     return false;
+    // }
 }
--- src/NumbersNMath/ReverseInteger.java
+++ src/NumbersNMath/ReverseInteger.java
@@ -1,19 +1,38 @@
 package NumbersNMath;
 
-// 7. Reverse Integer - Medium
-// https://leetcode.com/problems/reverse-integer/
-// Related Topics - Math
+// https://leetcode.com/problems/reverse-integer/ - Medium
+// Runtime 1 ms Beats 100%
+// Memory 39.4 MB Beats 88.37%
+// January 10, 2023
 
 public class ReverseInteger {
-    public int reverse(int x) {        
-        long ans=0;
+    public int reverse(int x) {
+        if(x>-10 && x<10){return x;}
+
+        int ans=0;
+        int digit=0;
         while(x!=0){
-            ans=ans*10+x%10;
+            digit=x%10;
             x/=10;
+
+            if (ans<Integer.MIN_VALUE/10 || (ans==Integer.MIN_VALUE/10 && digit<-8) || ans>Integer.MAX_VALUE/10 || (ans==Integer.MAX_VALUE/10 && digit>7)) {
+                return 0;
+            }
+            ans=ans*10+digit;
         }
-        if(ans>Integer.MAX_VALUE || ans<Integer.MIN_VALUE){
-            return 0;
-        }
-        return (int)ans;
+
+        return ans;
     }
+    
+    // public int reverse(int x) {        
+    //     long ans=0;
+    //     while(x!=0){
+    //         ans=ans*10+x%10;
+    //         x/=10;
+    //     }
+    //     if(ans>Integer.MAX_VALUE || ans<Integer.MIN_VALUE){
+    //         return 0;
+    //     }
+    //     return (int)ans;
+    // }
 }
