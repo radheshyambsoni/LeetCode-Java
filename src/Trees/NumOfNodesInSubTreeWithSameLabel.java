cc73464ad2303ba971c1e41529107db8180da81b
Radheshyam B. Soni <radheshyambsoni@gmail.com>
2023-01-12 22:06:29 +0530

    Added January 12, 2023 submission(s)

---
+++ src/Trees/NumOfNodesInSubTreeWithSameLabel.java
@@ -0,0 +1,45 @@
+package Trees;
+
+import java.util.ArrayList;
+
+// https://leetcode.com/problems/number-of-nodes-in-the-sub-tree-with-the-same-label/ - Medium
+// Runtime 57 ms Beats 100%
+// Memory 115.1 MB Beats 95.65%
+// January 12, 2023 - Daily Challenge
+
+public class NumOfNodesInSubTreeWithSameLabel {    
+    ArrayList<Integer>[] adjL; //adjacency list
+    char[] l;
+    int[] ans;
+    public int[] countSubTrees(int n, int[][] edges, String labels) {
+        adjL=new ArrayList[n];
+        for(int i=0;i<n;i++){
+            adjL[i]=new ArrayList<>();
+        }
+        
+        l=labels.toCharArray();
+        ans=new int[n]; //answer
+
+        for(int[] i:edges){
+            adjL[i[0]].add(i[1]);
+            adjL[i[1]].add(i[0]);
+        }
+        
+        dfs(-1,0);
+        
+        return ans;
+    }
+    int[] dfs(int prev,int curr){
+        int[] c=new int[26];
+        for(int i:adjL[curr]){
+            if(prev!=i){
+                int[] temp=dfs(curr,i);
+                for(int j=0;j<c.length;j++){
+                    c[j]+=temp[j];
+                }
+            }
+        }
+        ans[curr]=++c[l[curr]-'a'];
+        return c;
+    }
+}
