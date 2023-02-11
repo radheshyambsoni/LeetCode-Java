b4952cf988ea650fec9177c0f92e8603c5928e41
Radheshyam Soni <radheshyambsoni@gmail.com>
2023-02-11 11:48:13 +0530

    Added February 11, 2023 submission(s)

---
+++ src/DP/Easy/GetMaxInGeneratedArr.java
@@ -0,0 +1,27 @@
+package DP.Easy;
+
+// 1646. Get Maximum in Generated Array
+// https://leetcode.com/problems/get-maximum-in-generated-array/
+// Runtime 0 ms Beats 100%
+// Memory 38.9 MB Beats 86.73%
+// February 11, 2023
+
+public class GetMaxInGeneratedArr {
+    public int getMaximumGenerated(int n) {
+        if(n==0 || n==1){
+            return n;
+        }
+        int[] dp=new int[n+1];
+        dp[0]=1;dp[1]=1;
+        int max=1;
+        for(int i=2;i<=n;i++){
+            if(i%2==0){
+                dp[i]=dp[i/2];
+            }else{
+                dp[i]=dp[i/2]+dp[i/2+1];
+            }
+            max=Math.max(max,dp[i]);
+        }
+        return max;
+    }
+}
