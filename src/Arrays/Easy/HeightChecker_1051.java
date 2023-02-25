0a615f68195d95876cdf431cde0febfc3d8fb276
Radheshyam Soni <radheshyambsoni@gmail.com>
2023-02-25 15:38:07 +0530

    Added question #1051

---
+++ src/Arrays/Easy/HeightChecker_1051.java
@@ -0,0 +1,20 @@
+package Arrays.Easy;
+
+// 1051. Height Checker
+// https://leetcode.com/problems/height-checker/
+// Runtime 1 ms Beats 91.2%
+// Memory 40.4 MB Beats 23.37%
+// Feb 25, 2023
+
+public class HeightChecker_1051 {
+    public int heightChecker(int[] heights) {
+        int[] expected=new int[heights.length];
+        System.arraycopy(heights,0,expected,0,heights.length);
+        Arrays.sort(expected);
+        int count=0;
+        for(int i=0;i<heights.length;i++){
+            if(heights[i]!=expected[i]) count++;
+        }
+        return count;
+    }
+}
