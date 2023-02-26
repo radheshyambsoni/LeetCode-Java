7a4e4bf024df21012bffc93178236c0eb7ffd818
Radheshyam Soni <radheshyambsoni@gmail.com>
2023-02-26 22:42:06 +0530

    Added question #2236

---
+++ src/Trees/Easy/RootEqualsSumOfChildren.java
@@ -0,0 +1,13 @@
+package Trees.Easy;
+
+// 2236. Root Equals Sum of Children
+// https://leetcode.com/problems/root-equals-sum-of-children/
+// Runtime 0 ms Beats 100%
+// Memory 39.9 MB Beats 81.92%
+// Feb 26, 2023
+
+public class RootEqualsSumOfChildren {
+    public boolean checkTree(TreeNode root) {
+        return root.val==root.left.val+root.right.val;
+    }
+}
