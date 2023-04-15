1c83a231f89b0a4ed418a6ab81e1e81b9a59158d
Radheshyam Soni <radheshyambsoni@gmail.com>
2023-04-15 10:41:31 +0530

    Updated :rocket: question #513

--- src/Trees/Medium/FindBottomLeftTreeValue_513.java
+++ src/Trees/Medium/FindBottomLeftTreeValue_513.java
@@ -2,9 +2,9 @@ package Trees.Medium;
 
 // 513. Find Bottom Left Tree Value
 // https://leetcode.com/problems/find-bottom-left-tree-value/
-// Runtime 2 ms Beats 26.49%
-// Memory 42.6 MB Beats 15.93%
-// Apr 14, 2023
+// Runtime 1 ms Beats 67.94%
+// Memory 42.5 MB Beats 30.88%
+// Apr 15, 2023
 
 public class FindBottomLeftTreeValue_513 {
     public int findBottomLeftValue(TreeNode root) {
@@ -13,19 +13,17 @@ public class FindBottomLeftTreeValue_513 {
         int ans=root.val;
         while(!q.isEmpty()){
             int l=q.size();
-            LinkedList<Integer> ll=new LinkedList<>();
             for(int i=0;i<l;i++){
                 TreeNode n=q.poll();
-                if(n.left!=null){
-                    q.offer(n.left);
-                    ll.add(n.left.val);
-                }
                 if(n.right!=null){
                     q.offer(n.right);
-                    ll.add(n.right.val);
+                    ans=n.right.val;
+                }
+                if(n.left!=null){
+                    q.offer(n.left);
+                    ans=n.left.val;
                 }
             }
-            if(ll.size()!=0) ans=ll.get(0);
         }
         return ans;
     }
