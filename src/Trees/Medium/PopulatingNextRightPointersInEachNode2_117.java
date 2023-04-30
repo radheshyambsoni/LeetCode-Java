3a661cab5e90ea8e4845d6b0507548b106038c4d
Radheshyam Soni <radheshyambsoni@gmail.com>
2023-04-30 23:10:26 +0530

    Added :sparkles: question #117

---
+++ src/Trees/Medium/PopulatingNextRightPointersInEachNode2_117.java
@@ -0,0 +1,31 @@
+package Trees.Medium;
+
+// 117. Populating Next Right Pointers in Each Node II
+// https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/
+// Runtime 1 ms Beats 69.31%
+// Memory 42.4 MB Beats 35.9%
+// Apr 30, 2023
+
+public class PopulatingNextRightPointersInEachNode2_117 {
+    public Node connect(Node root) {
+        if(root==null) return null;
+        Queue<Node> q=new LinkedList<>();
+        q.offer(root);
+        Node prev=null;
+        while(!q.isEmpty()){
+            int l=q.size();
+            for(int i=0;i<l;i++){
+                Node n=q.poll();
+                if(i>0) prev.next=n;
+                if(n.left!=null){
+                    q.offer(n.left);
+                }
+                if(n.right!=null){
+                    q.offer(n.right);
+                }
+                prev=n;
+            }
+        }
+        return root;
+    }
+}
