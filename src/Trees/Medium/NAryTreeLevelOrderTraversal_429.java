bc97d11936bd3563b4db6a1834bc82452d509ffb
Radheshyam Soni <geekyradhe11@gmail.com>
2023-05-12 22:44:04 +0530

    Added :sparkles: question #429

---
+++ src/Trees/Medium/NAryTreeLevelOrderTraversal_429.java
@@ -0,0 +1,49 @@
+package Trees.Medium;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+// 429. N-ary Tree Level Order Traversal
+// https://leetcode.com/problems/n-ary-tree-level-order-traversal/
+// Runtime 3 ms Beats 80.79%
+// Memory 43.5 MB Beats 66.97%
+// May 12, 2023
+
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+
+public class NAryTreeLevelOrderTraversal_429 {
+    public List<List<Integer>> levelOrder(Node root) {
+        if(root==null) return new LinkedList<>();
+        Queue<Node> q=new LinkedList<>();
+        q.add(root);
+        List<List<Integer>> ans=new LinkedList<>();
+        while(!q.isEmpty()){
+            int level=q.size();
+            List<Integer> l=new LinkedList<>();
+            for(int i=0;i<level;i++){
+                l.add(q.peek().val);
+                List<Node> children=q.poll().children;
+                for(Node j:children){
+                    if(j!=null) q.offer(j);
+                }
+            }
+            ans.add(l);
+        }
+        return ans;
+    }
+}
