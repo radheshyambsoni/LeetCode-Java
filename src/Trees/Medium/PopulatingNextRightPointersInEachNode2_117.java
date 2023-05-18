c9c0ec37109af22340ff9bdd323054dda9c8f1cb
Radheshyam Soni <geekyradhe11@gmail.com>
2023-05-18 23:28:31 +0530

    Fixed :wrench: some files

--- src/Trees/Medium/PopulatingNextRightPointersInEachNode2_117.java
+++ src/Trees/Medium/PopulatingNextRightPointersInEachNode2_117.java
@@ -10,6 +10,26 @@ import java.util.Queue;
 // Apr 30, 2023
 
 public class PopulatingNextRightPointersInEachNode2_117 {
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+    
+        public Node() {}
+        
+        public Node(int _val) {
+            val = _val;
+        }
+    
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+    
     public Node connect(Node root) {
         if(root==null) return null;
         Queue<Node> q=new LinkedList<>();
--- src/Trees/Medium/PopulatingNextRightPtrsInEachNode_116.java
+++ src/Trees/Medium/PopulatingNextRightPtrsInEachNode_116.java
@@ -8,7 +8,27 @@ import java.util.LinkedList;
 // Memory 42.5 MB Beats 62.90%
 // Mar 31, 2023
 
+
 public class PopulatingNextRightPtrsInEachNode_116 {
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+    
+        public Node() {}
+        
+        public Node(int _val) {
+            val = _val;
+        }
+    
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
     public Node connect(Node root) {
         if(root==null) return null;
         root.next=null;
