95c3fe772a37d1a8b3cf19f5d3a0cddf1aa6721c
Radheshyam Soni <geekyradhe11@gmail.com>
2023-06-23 20:20:22 +0530

    Added :sparkles: question #124

---
+++ src/Trees/Hard/BinaryTreeMaxPathSum_124.java
@@ -0,0 +1,33 @@
+package Trees.Hard;
+
+// 124. BinaryTreeMaxPathSum_124
+// https://leetcode.com/problems/binary-tree-maximum-path-sum/
+// Runtime 1 ms Beats 91.24%
+// Memory 43.3 MB  Beats 99.76%
+// June 23, 2023
+
+public class BinaryTreeMaxPathSum_124 {
+    
+}
+class Solution {
+    int max=-10000;
+    public int maxPathSum(TreeNode root) {
+        helper(root);
+        return max;
+    }
+    int helper(TreeNode node){
+        if(node == null) {
+            return 0;
+        }
+
+        int leftSum = node.left==null?-1000:helper(node.left);
+        int rightSum = node.right==null?-1000:helper(node.right);
+        
+        int max1 = leftSum + rightSum + node.val;        
+
+        int retMax=Math.max(node.val, node.val+Math.max(leftSum, rightSum));
+
+        max=Math.max(max, Math.max(retMax, Math.max(max1, Math.max(leftSum, rightSum))));
+        return retMax;
+    }
+}
