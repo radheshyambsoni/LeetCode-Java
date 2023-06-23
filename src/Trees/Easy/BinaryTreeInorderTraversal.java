18d397e659aeb4f810c0db973fdd58d1923cf1bf
Radheshyam Soni <geekyradhe11@gmail.com>
2023-06-24 01:44:07 +0530

    Updated :sparkles: question #94

--- src/Trees/Easy/BinaryTreeInorderTraversal.java
+++ src/Trees/Easy/BinaryTreeInorderTraversal.java
@@ -2,25 +2,55 @@ package Trees.Easy;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 // https://leetcode.com/problems/binary-tree-inorder-traversal/
+// 94. Binary Tree Inorder Traversal
 // Runtime 0 ms Beats 100%
-// Memory 40.5 MB Beats 73.23%
-// January 13, 2023
+// Memory 40.8 MB Beats 78.6%
+// June 24, 2023
 
+// Iterative solution
 public class BinaryTreeInorderTraversal {
-    List<Integer> ans=new ArrayList<>();
     public List<Integer> inorderTraversal(TreeNode root) {
-        if(root==null){return new ArrayList<>();}
-        helper(root);
-        return ans;
-    }
-    void helper(TreeNode node){
-        if(node==null){
-            return;
+        if(root==null) return new ArrayList<>();
+        List<Integer> inorder=new ArrayList<>();
+        Stack<TreeNode> st=new Stack<>();
+        TreeNode node=root;
+        while(true){
+            if(node!=null){
+                st.push(node);
+                node=node.left;
+            }else{
+                if(st.isEmpty()) break;
+                node=st.pop();
+                inorder.add(node.val);
+                node=node.right;
+            }
         }
-        helper(node.left);
-        ans.add(node.val);
-        helper(node.right);
+        
+        return inorder;
     }
 }
+
+// // Runtime 0 ms Beats 100%
+// // Memory 40.5 MB Beats 73.23%
+// // January 13, 2023
+
+// Recursive Solution
+// public class BinaryTreeInorderTraversal {
+//     List<Integer> ans=new ArrayList<>();
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         if(root==null){return new ArrayList<>();}
+//         helper(root);
+//         return ans;
+//     }
+//     void helper(TreeNode node){
+//         if(node==null){
+//             return;
+//         }
+//         helper(node.left);
+//         ans.add(node.val);
+//         helper(node.right);
+//     }
+// }
