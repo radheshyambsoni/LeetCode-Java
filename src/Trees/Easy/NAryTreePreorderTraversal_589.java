19e2a9c7487667bc28b185af4eaea40c43d3bd4a
Radheshyam Soni <geekyradhe11@gmail.com>
2023-06-24 14:45:38 +0530

    Updated :sparkles: question #589

---
+++ src/Trees/Easy/NAryTreePreorderTraversal_589.java
@@ -0,0 +1,68 @@
+package Trees.Easy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+// 589. N-ary Tree Preorder Traversal
+// https://leetcode.com/problems/n-ary-tree-preorder-traversal/description/
+
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+    
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+// Runtime 3 ms Beats 29.70%
+// Memory 44.6 MB Beats 14.31%
+// June 24, 2023
+
+// Iterative Solution
+public class NAryTreePreorderTraversal_589 {
+    public List<Integer> preorder(Node root) {
+        List<Integer> pre=new ArrayList<>();
+        if(root==null) return pre;
+        Stack<Node> st=new Stack<>();
+        st.push(root);
+        while(!st.isEmpty()){
+            Node temp=st.pop();
+            pre.add(temp.val);
+            for(int i=temp.children.size()-1;i>=0;i--){
+                Node n=temp.children.get(i);
+                st.push(n);
+            }
+        }
+        return pre;
+    }
+}
+
+// Runtime 1 ms Beats 87.94%
+// Memory 44.5 MB Beats 26.12%
+// June 24, 2023
+
+// Recursive Solution
+// public class NAryTreePreorderTraversal_589 {
+//     public List<Integer> preorder(Node root) {
+//         List<Integer> pre=new ArrayList<>();
+//         if(root==null) return pre;
+//         helper(root,pre);
+//         return pre;
+//     }
+//     void helper(Node node,List<Integer> pre){
+//         pre.add(node.val);
+//         for(Node n:node.children){
+//             if(n!=null) helper(n,pre);
+//         }
+//     }
+// }
