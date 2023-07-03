8d6d1e397c97397f683039614377520c4505b230
Radheshyam Soni <geekyradhe11@gmail.com>
2023-07-03 20:17:22 +0530

    Updated :sparkles: question #416

--- src/DP/Medium/PartitionEqualSubsetSum_416.java
+++ src/DP/Medium/PartitionEqualSubsetSum_416.java
@@ -2,34 +2,34 @@ package DP.Medium;
 
 // 416. Partition Equal Subset Sum
 // https://leetcode.com/problems/partition-equal-subset-sum/description/
-// Runtime 66 ms Beats 47.39%
-// Memory 43.7 MB Beats 75.6%
+// Runtime 46 ms Beats 70.20% 
+// Memory 44 MB Beats 76.40%
+// Jul 03, 2023
 
 public class PartitionEqualSubsetSum_416 {
-    static boolean fSpaceOptim(int[] arr,int target){
-        boolean[] dp=new boolean[target+1];
+    public boolean canPartition(int[] nums) {
+        int totSum=0;
+        for(int i:nums) totSum+=i;
+        if(totSum%2!=0) return false;
+        totSum/=2;
+        
+        boolean[] dp=new boolean[totSum+1];
         dp[0]=true;
-        if(arr[0]<=target) dp[arr[0]]=true;
+        if(nums[0]<=totSum) dp[nums[0]]=true;
 
-        for(int idx=1;idx<arr.length;idx++){
-            boolean[] temp=new boolean[target+1];
+        for(int idx=1;idx<nums.length;idx++){
+            boolean[] temp=new boolean[totSum+1];
             temp[0]=true; // from base case condition
-            for(int k=1;k<=target;k++){
-                boolean notTake=dp[k];
+            for(int tar=1;tar<=totSum;tar++){
+                boolean notTake=dp[tar];
                 boolean take=false;
-                if(arr[idx]<=k){
-                    take=dp[k-arr[idx]];
+                if(nums[idx]<=tar){
+                    take=dp[tar-nums[idx]];
                 }
-                temp[k] = (take || notTake);
+                temp[tar] = (take | notTake);
             }
             dp=temp;
         }
-        return dp[target];
-    }
-    public boolean canPartition(int[] nums) {
-        int totalSum=0;
-        for(int i:nums) totalSum+=i;
-        if(totalSum%2!=0) return false;
-        return fSpaceOptim(nums,totalSum/2);
+        return dp[totSum];
     }
 }
