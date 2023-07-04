628b259187955b6838cc01e218a7dea59384d181
Radheshyam Soni <geekyradhe11@gmail.com>
2023-07-04 19:51:47 +0530

    Updated :sparkles: question #322

--- src/DP/Medium/CoinChange_322.java
+++ src/DP/Medium/CoinChange_322.java
@@ -2,8 +2,8 @@ package DP.Medium;
 
 // 322. Coin Change
 // https://leetcode.com/problems/coin-change/
-// Runtime 426 ms Beats 5.11%
-// Memory 44.1 MB Beats 15.72%
+// Runtime 20 ms Beats 57.3%
+// Memory 43.8 MB Beats 28.43%
 // Jul 04, 2023
 
 public class CoinChange_322 {
@@ -12,22 +12,21 @@ public class CoinChange_322 {
 
         for(int amt=1;amt<=amount;amt++){
             if(amt%coins[0]==0) dp[amt]=amt/coins[0];
-            else dp[amt]=-1;
+            else dp[amt]=(int)1e9;
         }
 
         for(int idx=1;idx<coins.length;idx++){
             int[] temp=new int[amount+1];
             for(int amt=0;amt<=amount;amt++){
-                int quo=amt/coins[idx];
-                int cnt=(int)1e9;
-                for(int i=quo;i>=0;i--){
-                    int prev=dp[amt-i*coins[idx]];
-                    cnt=Math.min(cnt,i+(prev==-1?(int)1e9:prev));
+                int notTake=dp[amt];
+                int take=Integer.MAX_VALUE;
+                if(coins[idx]<=amt){
+                    take=1+temp[amt-coins[idx]];
                 }
-                temp[amt]=cnt!=(int)1e9?cnt:-1;
+                temp[amt]=Math.min(take,notTake);
             }
             dp=temp;
         }
-        return dp[amount];
+        return dp[amount]>=1e9?-1:dp[amount];
     }
 }
