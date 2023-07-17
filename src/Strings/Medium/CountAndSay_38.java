2f378d835e982346b79214efd14f5b018c77a460
Radheshyam Soni <geekyradhe11@gmail.com>
2023-07-17 15:14:13 +0530

    Added :sparkles: question #38

---
+++ src/Strings/Medium/CountAndSay_38.java
@@ -0,0 +1,31 @@
+package Strings.Medium;
+
+// 38. Count and Say
+// https://leetcode.com/problems/count-and-say/
+// Runtime 6 ms Beats 59.63%
+// Memory 40.1 MB Beats 84.19%
+
+public class CountAndSay_38 {
+    public String countAndSay(int n) {
+        String curr="1";        
+        while(--n>0){
+            int count=1;
+            char prev=curr.charAt(0);
+            StringBuilder sb=new StringBuilder();
+            for(int i=1;i<curr.length();i++){
+                if(curr.charAt(i)==prev){
+                    count++;
+                }else{
+                    sb.append(count);
+                    sb.append(prev);
+                    count=1;
+                    prev=curr.charAt(i);
+                }
+            }
+            sb.append(count);
+            sb.append(prev);
+            curr=sb.toString();
+        }
+        return curr;
+    }
+}
