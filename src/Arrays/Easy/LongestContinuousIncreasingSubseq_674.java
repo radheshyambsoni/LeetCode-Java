804a0dca6a8f4bd3c30643ee9bfce8fdf5b65bb8
Radheshyam Soni <geekyradhe11@gmail.com>
2023-07-19 19:28:50 +0530

    Updated :sparkles: question #674

---
+++ src/Arrays/Easy/LongestContinuousIncreasingSubseq_674.java
@@ -0,0 +1,22 @@
+package Arrays.Easy;
+
+// 674. Longest Continuous Increasing Subsequence
+// https://leetcode.com/problems/longest-continuous-increasing-subsequence/
+// Runtime 1 ms Beats 98.63%
+// Memory 44 MB Beats 53.97%
+
+public class LongestContinuousIncreasingSubseq_674 {
+    public int findLengthOfLCIS(int[] nums) {
+        int max = 1;
+        int cnt = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                cnt++;
+                max = Math.max(max, cnt);
+            } else {
+                cnt = 1;
+            }
+        }
+        return max;
+    }
+}
