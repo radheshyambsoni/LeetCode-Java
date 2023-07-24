6f9741e782050dfcb81382d040b3520e7b3d02ce
Radheshyam Soni <geekyradhe11@gmail.com>
2023-07-24 21:23:44 +0530

    Updated :sparkles: question #50

--- src/Recursion/Medium/PowXN.java
+++ src/Recursion/Medium/PowXN.java
@@ -2,29 +2,32 @@ package Recursion.Medium;
 
 // 50. Pow(x, n)
 // https://leetcode.com/problems/powx-n/
-// Runtime 1 ms Beats 87.9%
-// Memory 43.6 MB Beats 20.53%
+// Runtime 0 ms Beats 100%
+// Memory 43.2 MB Beats 5.76%
 
 public class PowXN {
     public double myPow(double x, int n) {
-        if(x==0){return 0;}
-        if(n==0){return 1;}        
-        return helper(x,n);
+        if (x == 0) {
+            return 0;
+        }
+        if (n == 0) {
+            return 1;
+        }
+
+        if (n < 0) {
+            return 1 / helper(x, n * -1);
+        }
+
+        return helper(x, n);
     }
+
     private double helper(double x, int n) {
-        if(n==1){
-            return x;
+        if (n == 0)
+            return 1;
+        double temp = helper(x, n / 2);
+        if (n % 2 == 0) {
+            return temp * temp;
         }
-        if(n==-1){
-            return 1/x;
-        }
-        double temp=helper(x,n/2);
-        if(n%2==0){            
-            return temp*temp;
-        }
-        if(n>0){
-            return temp*temp*x;
-        }
-        return temp*temp*(1/x);
+        return temp * temp * x;
     }
 }
