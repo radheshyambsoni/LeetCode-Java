850692987247867cf3b6dd94c1640ab351c73eda
Radheshyam Soni <geekyradhe11@gmail.com>
2023-07-28 22:41:24 +0530

    Updated :sparkles: question #486

--- src/DP/Medium/PredictTheWinner_486.java
+++ src/DP/Medium/PredictTheWinner_486.java
@@ -5,26 +5,48 @@ package DP.Medium;
 // 486. Predict the Winner
 // https://leetcode.com/problems/predict-the-winner/
 // Runtime 0 ms Beats 100%
-// Memory 39.6 MB Beats 96.57%
+// Memory 39.9 MB Beats 77.43%
 // Jul 28, 2023
 
+// Space Optimization
 public class PredictTheWinner_486 {
     public boolean PredictTheWinner(int[] nums) {
-        int[][] dp = new int[nums.length][nums.length];
+        int[] dp = new int[nums.length];
         // base case
         for (int i = 0; i < nums.length; i++)
-            dp[i][i] = nums[i];
+            dp[i] = nums[i];
 
         for (int diff = 1; diff < nums.length; diff++) {
             for (int i = 0; i < nums.length - diff; i++) {
                 int j = i + diff;
-                dp[i][i + diff] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+                dp[i] = Math.max(nums[i] - dp[i + 1], nums[j] - dp[i]);
             }
         }
 
-        return dp[0][nums.length - 1] >= 0;
+        return dp[0] >= 0;
     }
 }
+
+// // Tabulation
+// // Runtime 0 ms Beats 100%
+// // Memory 39.6 MB Beats 96.57%
+// public class PredictTheWinner_486 {
+// public boolean PredictTheWinner(int[] nums) {
+// int[][] dp = new int[nums.length][nums.length];
+// // base case
+// for (int i = 0; i < nums.length; i++)
+// dp[i][i] = nums[i];
+
+// for (int diff = 1; diff < nums.length; diff++) {
+// for (int i = 0; i < nums.length - diff; i++) {
+// int j = i + diff;
+// dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+// }
+// }
+
+// return dp[0][nums.length - 1] >= 0;
+// }
+// }
 
 // // memoization
 // // Runtime 0 ms Beats 100%
