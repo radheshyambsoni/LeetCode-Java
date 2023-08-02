a08cd558f5d14ece18925b75a60a8bfe7b3a8d11
Radheshyam Soni <geekyradhe11@gmail.com>
2023-08-02 14:55:19 +0530

    Updated :sparkles: question #46

--- src/BackTracking/Medium/Permutations.java
+++ src/BackTracking/Medium/Permutations.java
@@ -3,38 +3,42 @@ package BackTracking.Medium;
 import java.util.List;
 import java.util.ArrayList;
 
+// 46. Permutations
 // https://leetcode.com/problems/permutations/
 
-public class Permutations{
+public class Permutations {
     // Runtime 1 ms Beats 95.48%
     // Memory 44.3 MB Beats 57.58%
     // January 03, 2023
     // No space taken for tracking the used elements
-    List<List<Integer>> ans=new ArrayList<>();
-    public List<List<Integer>> permute(int[] arr){
-        addPermutations(arr,0);
+    List<List<Integer>> ans = new ArrayList<>();
+
+    public List<List<Integer>> permute(int[] arr) {
+        addPermutations(arr, 0);
         return ans;
     }
-    private void addPermutations(int[] arr,int index) {
-        if(index==arr.length){
-            List<Integer> ds=new ArrayList<>();
-            for(int i:arr){
+
+    private void addPermutations(int[] arr, int index) {
+        if (index == arr.length) {
+            List<Integer> ds = new ArrayList<>();
+            for (int i : arr) {
                 ds.add(i);
             }
             ans.add(ds);
             return;
         }
 
-        for(int i=index;i<arr.length;i++){
-            swap(arr,index,i);
-            addPermutations(arr, index+1);
-            swap(arr,index,i);
+        for (int i = index; i < arr.length; i++) {
+            swap(arr, index, i);
+            addPermutations(arr, index + 1);
+            swap(arr, index, i);
         }
     }
+
     private void swap(int[] arr, int index, int i) {
-        int temp=arr[index];
-        arr[index]=arr[i];
-        arr[i]=temp;
+        int temp = arr[index];
+        arr[index] = arr[i];
+        arr[i] = temp;
     }
 
     // O(n) space complexity for tracking the elements used
@@ -43,24 +47,24 @@ public class Permutations{
     // // December 13, 2022
     // List<List<Integer>> ans=new ArrayList<>();
     // public List<List<Integer>> permute(int[] nums) {
-    //     addPermutations(nums, new boolean[nums.length], new ArrayList<>());
-    //     return ans;
+    // addPermutations(nums, new boolean[nums.length], new ArrayList<>());
+    // return ans;
     // }
     // public void addPermutations(int[] nums,boolean[] track,List<Integer> l){
-    //     if(l.size()==nums.length){
-    //         List<Integer> a=new ArrayList<>(l);
-    //         ans.add(a);
-    //         return;
-    //     }
+    // if(l.size()==nums.length){
+    // List<Integer> a=new ArrayList<>(l);
+    // ans.add(a);
+    // return;
+    // }
 
-    //     for(int i=0;i<nums.length;i++){
-    //         if(!track[i]){
-    //             track[i]=true;
-    //             l.add(nums[i]);
-    //             addPermutations(nums, track, l);
-    //             l.remove(l.size()-1);
-    //             track[i]=false;
-    //         }
-    //     }
+    // for(int i=0;i<nums.length;i++){
+    // if(!track[i]){
+    // track[i]=true;
+    // l.add(nums[i]);
+    // addPermutations(nums, track, l);
+    // l.remove(l.size()-1);
+    // track[i]=false;
+    // }
+    // }
     // }
 }
