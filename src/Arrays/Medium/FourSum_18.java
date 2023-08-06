189d7aa64beddd959b252514c296448a28936620
Radheshyam Soni <geekyradhe11@gmail.com>
2023-08-06 23:37:11 +0530

    Added :sparkles: question #18

---
+++ src/Arrays/Medium/FourSum_18.java
@@ -0,0 +1,48 @@
+package Arrays.Medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// 18. 4Sum
+// https://leetcode.com/problems/4sum/
+// Runtime 20 ms Beats 76.54%
+// Memory 44 MB Beats 74.94%
+// Aug 06, 2023
+public class FourSum_18 {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
+                int k = j + 1;
+                int l = n - 1;
+                while (k < l) {
+                    long sum = nums[i];
+                    sum += nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+                    if (sum == target) {
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+                        k++;
+                        l--;
+                        while (k < l && nums[k - 1] == nums[k])
+                            k++;
+                        while (k < l && nums[l] == nums[l + 1])
+                            l--;
+                    } else if (sum < target)
+                        k++;
+                    else
+                        l--;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
