f123b1aa471f8af7d200ab5987686be351e34c18
Radheshyam Soni <geekyradhe11@gmail.com>
2023-08-23 02:39:41 +0530

    Updated :sparkles: question #33

--- src/BinarySearch/Medium/SearchInRotatedSortedArr_33.java
+++ src/BinarySearch/Medium/SearchInRotatedSortedArr_33.java
@@ -3,55 +3,40 @@ package BinarySearch.Medium;
 // 33. Search in Rotated Sorted Array
 // https://leetcode.com/problems/search-in-rotated-sorted-array/
 // Runtime 0 ms Beats 100%
-// Memory 41.8 MB Beats 5.88%
-// Aug 08, 2023
+// Memory 41.2 MB Beats 28.50%
+// Aug 23, 2023
 
 public class SearchInRotatedSortedArr_33 {
-    int binarySearch(int nums[], int target, int start, int end) {
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] > target) {
-                end = mid - 1;
-            } else if (nums[mid] < target) {
-                start = mid + 1;
-            } else {
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target)
                 return mid;
+            // if left portion is sorted
+            if (nums[low] <= nums[mid]) {
+                // if the current portion contains the target
+                if (nums[low] <= target && target <= nums[mid]) {
+                    high = mid - 1;
+                }
+                // if the current portion does not contain the target
+                else {
+                    low = mid + 1;
+                }
+            }
+            // if right portion is sorted
+            else {
+                // if the current portion contains the target
+                if (nums[mid] <= target && target <= nums[high]) {
+                    low = mid + 1;
+                }
+                // if the current portion does not contain the target
+                else {
+                    high = mid - 1;
+                }
             }
         }
-        return -1;
-    }
 
-    int search(int[] nums, int target) {
-        int pivot = findPivot(nums);
-        if (pivot == -1) {
-            return binarySearch(nums, target, 0, nums.length - 1);
-        }
-
-        if (nums[pivot] == target) {
-            return pivot;
-        }
-        if (target >= nums[0]) {
-            return binarySearch(nums, target, 0, pivot - 1);
-        }
-        return binarySearch(nums, target, pivot + 1, nums.length - 1);
-    }
-
-    public int findPivot(int[] arr) {
-        int start = 0, end = arr.length - 1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (mid < end && arr[mid] > arr[mid + 1]) {
-                return mid;
-            }
-            if (mid > start && arr[mid] < arr[mid - 1]) {
-                return mid - 1;
-            }
-            if (arr[mid] < arr[start]) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
-        }
         return -1;
     }
 }
