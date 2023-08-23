ab102e636f4a6e4ac29e6d82a230b0d10eab9027
Radheshyam Soni <geekyradhe11@gmail.com>
2023-08-23 15:28:48 +0530

    Updated :sparkles: question #81

--- src/BinarySearch/Medium/SearchInRotatedSortedArray2_81.java
+++ src/BinarySearch/Medium/SearchInRotatedSortedArray2_81.java
@@ -3,68 +3,37 @@ package BinarySearch.Medium;
 // 81. Search in Rotated Sorted Array II
 // https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
 // Runtime 0 ms Beats 100%
-// Memory 42.7 MB Beats 91.85%
-// Aug 10, 2023
+// Memory 43 MB Beats 53.67%
+// Aug 23, 2023
 
 public class SearchInRotatedSortedArray2_81 {
-    public boolean binarySearch(int nums[], int target, int start, int end) {
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] > target) {
-                end = mid - 1;
-            } else if (nums[mid] < target) {
-                start = mid + 1;
-            } else {
-                return true;
-            }
-        }
-        return false;
-    }
-
     public boolean search(int[] nums, int target) {
-        int pivot = findPivotWithDuplicates(nums);
-        if (pivot == -1) {
-            return binarySearch(nums, target, 0, nums.length - 1);
-        }
-
-        if (nums[pivot] == target) {
-            return true;
-        }
-        if (target >= nums[0]) {
-            return binarySearch(nums, target, 0, pivot - 1);
-        }
-        return binarySearch(nums, target, pivot + 1, nums.length - 1);
-    }
-
-    public int findPivotWithDuplicates(int[] arr) {
-        int start = 0, end = arr.length - 1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (mid < arr.length - 1 && arr[mid] > arr[mid + 1]) {
-                return mid;
-            }
-            if (mid > 0 && arr[mid] < arr[mid - 1]) {
-                return mid - 1;
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target)
+                return true;
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                low++;
+                high--;
+                continue;
             }
 
-            // if elements at middle, start, and end are equal then just skip the duplicated
-            if (arr[mid] == arr[start] && arr[mid] == arr[end]) {
-                // check if the skipped ones are pivots
-                if (start < arr.length - 1 && arr[start] > arr[start + 1]) {
-                    return start;
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
                 }
-                start++;
-
-                if (end > 0 && arr[end] < arr[end - 1]) {
-                    return end - 1;
-                }
-                end--;
-            } else if (arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])) {
-                start = mid + 1;
             } else {
-                end = mid - 1;
+                if (nums[mid] <= target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
             }
         }
-        return -1;
+
+        return false;
     }
 }
