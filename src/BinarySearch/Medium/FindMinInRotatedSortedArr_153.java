02e4d038e74d1e6fe37be256cce9fd4bc57af833
Radheshyam Soni <geekyradhe11@gmail.com>
2023-08-23 19:37:31 +0530

    Updated :sparkles: question #153

--- src/BinarySearch/Medium/FindMinInRotatedSortedArr_153.java
+++ src/BinarySearch/Medium/FindMinInRotatedSortedArr_153.java
@@ -3,7 +3,7 @@ package BinarySearch.Medium;
 // 153. Find Minimum in Rotated Sorted Array
 // https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
 // Runtime 0 ms Beats 100%
-// Memory 40.7 MB Beats 81.89%
+// Memory 40.6 MB Beats 81.89%
 // Aug 23, 2023
 
 public class FindMinInRotatedSortedArr_153 {
@@ -12,6 +12,12 @@ public class FindMinInRotatedSortedArr_153 {
         int ans = Integer.MAX_VALUE;
         while (low <= high) {
             int mid = low + (high - low) / 2;
+            // Little optimisation
+            if (nums[low] <= nums[high]) {
+                ans = Math.min(ans, nums[low]);
+                break;
+            }
+
             if (nums[low] <= nums[mid]) {
                 ans = Math.min(ans, nums[low]);
                 low = mid + 1;
@@ -23,6 +29,27 @@ public class FindMinInRotatedSortedArr_153 {
         return ans;
     }
 }
+
+// // Runtime 0 ms Beats 100%
+// // Memory 40.7 MB Beats 81.89%
+// // Aug 23, 2023
+// public class FindMinInRotatedSortedArr_153 {
+// public int findMin(int[] nums) {
+// int low = 0, high = nums.length - 1;
+// int ans = Integer.MAX_VALUE;
+// while (low <= high) {
+// int mid = low + (high - low) / 2;
+// if (nums[low] <= nums[mid]) {
+// ans = Math.min(ans, nums[low]);
+// low = mid + 1;
+// } else {
+// ans = Math.min(ans, nums[mid]);
+// high = mid - 1;
+// }
+// }
+// return ans;
+// }
+// }
 
 // // Runtime 0 ms Beats 100%
 // // Memory 42.9 MB Beats 19.87%
