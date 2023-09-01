e2ecbf2e83f58018ff5c113a35f0fe1b7e53a25d
Radheshyam Soni <geekyradhe11@gmail.com>
2023-09-01 22:43:12 +0530

    Updated :sparkles: question #338

--- src/BitManipulation/Easy/CountingBits.java
+++ src/BitManipulation/Easy/CountingBits.java
@@ -1,26 +1,23 @@
 package BitManipulation.Easy;
 
-import java.util.Arrays;
-
+// 338. Counting Bits
 // https://leetcode.com/problems/counting-bits/
-// Runtime 3 ms Beats 77.29%
-// Memory 48.1 MB Beats 56.62%
+// Runtime 1 ms Beats 99.63%
+// Memory 46.4 MB Beats 51.44%
+// Sep 01, 2023
 
 public class CountingBits {
-    public static int[] countBits(int n) {
-        int[] ans=new int[n+1];
-        ans[0]=0;
-        int temp;
-        for(int i=1;i<=n;i++){
-            temp=i;
-            while(temp>0){
-                ans[i]++;
-                temp&=temp-1;
+    public int[] countBits(int n) {
+        int[] ans = new int[n + 1];
+        ans[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            int temp = i, cnt = 0;
+            while (temp > 0) {
+                temp &= temp - 1;
+                cnt++;
             }
+            ans[i] = cnt;
         }
         return ans;
-    }
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(countBits(6)));
     }
 }
