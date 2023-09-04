a2d356db3ff933d251f630859caac5bc1484292c
Radheshyam Soni <geekyradhe11@gmail.com>
2023-09-04 16:47:02 +0530

    Updated :sparkles: question #141

--- src/LinkedList/Easy/LinkedListCycle_141.java
+++ src/LinkedList/Easy/LinkedListCycle_141.java
@@ -1,17 +1,18 @@
 package LinkedList.Easy;
 
+// 141. Linked List Cycle
 // https://leetcode.com/problems/linked-list-cycle/
 // Runtime 0 ms Beats 100%
 // Memory 45.8 MB Beats 47.34%
 // December 01, 2022
 
-public class LinkedListCycle {
+public class LinkedListCycle_141 {
     public boolean hasCycle(ListNode head) {
-        ListNode slow=head,fast=head;
-        while(fast!=null && fast.next!=null){
-            fast=fast.next.next;
-            slow=slow.next;
-            if(slow==fast){
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast) {
                 return true;
             }
         }
