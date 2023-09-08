b443363a5f0aa0ba7a5a4fe7d2664e1c5dfbef76
Radheshyam Soni <geekyradhe11@gmail.com>
2023-09-08 20:06:47 +0530

    Updated :sparkles: question #547

--- src/Graphs/Medium/NumberOfProvinces_547.java
+++ src/Graphs/Medium/NumberOfProvinces_547.java
@@ -3,21 +3,18 @@ package Graphs.Medium;
 // 547. Number of Provinces
 // https://leetcode.com/problems/number-of-provinces/
 // Runtime 1 ms Beats 96.16%
-// Memory 46.3 MB Beats 32.40%
+// Memory 46.7 MB Beats 8.28%
 // Sep 08, 2023
 
 public class NumberOfProvinces_547 {
-    boolean dfs(boolean[] visited, int city, int[][] conn) {
+    void dfs(boolean[] visited, int city, int[][] conn) {
         if (!visited[city]) {
             visited[city] = true;
             for (int i = 0; i < conn.length; i++) {
                 if (i != city && conn[city][i] == 1)
                     dfs(visited, i, conn);
             }
-            return true;
         }
-
-        return false;
     }
 
     public int findCircleNum(int[][] isConnected) {
@@ -26,8 +23,10 @@ public class NumberOfProvinces_547 {
 
         int cnt = 0;
         for (int i = 0; i < n; i++) {
-            if (!visited[i] && dfs(visited, i, isConnected))
+            if (!visited[i]) {
+                dfs(visited, i, isConnected);
                 cnt++;
+            }
         }
 
         return cnt;
