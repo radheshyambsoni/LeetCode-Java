197938a0afb0b03fe2f8ea83d3164cbe2a91b9dc
Radheshyam Soni <geekyradhe11@gmail.com>
2023-09-19 15:52:48 +0530

    Updated :sparkles: question #287

--- src/Arrays/Medium/FindTheDuplicateNumber.java
+++ src/Arrays/Medium/FindTheDuplicateNumber.java
@@ -1,60 +1,46 @@
 package Arrays.Medium;
 
+// 287. Find the Duplicate Number
 // https://leetcode.com/problems/find-the-duplicate-number/
+// Runtime 1 ms Beats 100%
+// Memory 57.7 MB Beats 15.74%
+// Sep 19, 2023
 
-public class FindTheDuplicateNumber{
+// O(n) space
+public class FindTheDuplicateNumber {
     public int findDuplicate(int[] nums) {
-        // December 06, 2022
-        // Linked List cycle detection method - where the start of cycle is our duplicate element
-        // cycle detection
-        int slow=nums[0];
-        int fast=nums[0];
-        do{
-            slow=nums[slow];
-            fast=nums[nums[fast]];
-        }while(slow!=fast);
-        
-        // cycle start detection
-        fast=nums[0];
-        while(slow!=fast){
-            slow=nums[slow];
-            fast=nums[fast];
+        int n = nums.length;
+        boolean[] hash = new boolean[n + 1];
+        for (int num : nums) {
+            if (hash[num])
+                return num;
+            else
+                hash[num] = true;
         }
-        return slow;
 
-
-        // Cyclic sort
-        // int i=0;
-        // while(i<nums.length){
-        //     if(nums[i]!=i+1){
-        //         int correctIdx=nums[i]-1;
-        //         if(nums[i]!=nums[correctIdx]){
-        //             int temp=nums[i];
-        //             nums[i]=nums[correctIdx];
-        //             nums[correctIdx]=temp;
-        //         }else{
-        //             return nums[i];
-        //         }                
-        //     }else{
-        //         i++;
-        //     }
-        // }
-        // return -1;
-        
-        
-        // Runtime: 2 ms, faster than 99.89% of Java online submissions for Find the Duplicate Number.
-        // Memory Usage: 58.4 MB, less than 99.43% of Java online submissions for Find the Duplicate Number.
-        // fastest one, generic one but uses space for boolean array
-        // int ans=-1;
-        // boolean[] check=new boolean[nums.length];
-        // for(int i=0;i<nums.length;i++){
-        //     if(!check[nums[i]]){
-        //         check[nums[i]]=true;
-        //     }else{
-        //         ans=nums[i];
-        //         break;
-        //     }
-        // }
-        // return ans;
+        return -1;
     }
 }
+
+// public class FindTheDuplicateNumber {
+// public int findDuplicate(int[] nums) {
+// // December 06, 2022
+// // Linked List cycle detection method - where the start of cycle is our
+// duplicate element
+// // cycle detection
+// int slow = nums[0];
+// int fast = nums[0];
+// do {
+// slow = nums[slow];
+// fast = nums[nums[fast]];
+// } while (slow != fast);
+
+// // cycle start detection
+// fast = nums[0];
+// while (slow != fast) {
+// slow = nums[slow];
+// fast = nums[fast];
+// }
+// return slow;
+// }
+// }
