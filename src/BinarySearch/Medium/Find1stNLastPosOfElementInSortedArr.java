d2a2a72ce1a90cfdb3860bae936b19cea4ca31d7
Radheshyam Soni <geekyradhe11@gmail.com>
2023-10-09 14:51:08 +0530

    Added :sparkles: question #34

--- src/BinarySearch/Medium/Find1stNLastPosOfElementInSortedArr.java
+++ src/BinarySearch/Medium/Find1stNLastPosOfElementInSortedArr.java
@@ -2,62 +2,36 @@ package BinarySearch.Medium;
 
 // 34. Find First and Last Position of Element in Sorted Array
 // https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
-// Related Topics - Arrays, Binary Search
+// Runtime 0 ms Beats 100%
+// Memory 44.4 MB Beats 54.18%
+// Sep 09, 2023
 
 public class Find1stNLastPosOfElementInSortedArr {
     public int[] searchRange(int[] nums, int target) {
-        // Runtime 0 ms Beats 100% Memory 45.5 MB Beats 92.37%
-        int ans[]={-1,-1};
-        ans[0]=search(nums,target,true);
-        ans[1]=search(nums,target,false);
+        int ans[] = { -1, -1 };
+        ans[0] = search(nums, target, true);
+        ans[1] = search(nums, target, false);
         return ans;
-
-        // int start=0,end=nums.length-1;        
-        // while(start<=end){
-        //     int mid=start+(end-start)/2;
-        //     if(nums[mid]==target){
-        //         start=mid;
-        //         end=mid;
-        //         break;       
-        //     }else if(nums[mid]>target){
-        //         end=mid-1;
-        //         mid=start+(end-mid)/2;
-        //     }else{
-        //         start=mid+1;
-        //         mid=start+(end-mid)/2;
-        //     }
-        // }
-        // if(start>end){
-        //     return new int[]{-1,-1};
-        // }
-        // while(start>=0 && nums[start]>=target){
-        //     start--;
-        // }
-        // while(end<nums.length && nums[end]<=target){
-        //     end++;
-        // }
-
-        // return new int[]{start+1,end-1};
     }
 
     int search(int[] nums, int target, boolean startOrEnd) {
-        int ans=-1,start=0,end=nums.length-1;
-        int mid=start+(end-start)/2;
-        while(start<=end){
-            if(target<nums[mid]){
-                end=mid-1;
-                mid=start+(end-start)/2;
-            }else if(target>nums[mid]){
-                start=mid+1;
-                mid=start+(end-start)/2;
-            }else{
-                ans=mid;
-                if(startOrEnd){
-                    end=mid-1;
-                    mid=start+(end-start)/2;
-                }else{
-                    start=mid+1;
-                    mid=start+(end-start)/2;
+        int ans = -1, start = 0, end = nums.length - 1;
+        int mid = start + (end - start) / 2;
+        while (start <= end) {
+            if (target < nums[mid]) {
+                end = mid - 1;
+                mid = start + (end - start) / 2;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+                mid = start + (end - start) / 2;
+            } else {
+                ans = mid;
+                if (startOrEnd) {
+                    end = mid - 1;
+                    mid = start + (end - start) / 2;
+                } else {
+                    start = mid + 1;
+                    mid = start + (end - start) / 2;
                 }
             }
         }
