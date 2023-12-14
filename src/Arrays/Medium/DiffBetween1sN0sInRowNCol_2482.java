08048c97d2d1578e83d733b4b9cb7aad78baa655
Radheshyam Soni <radheshyambsoni@gmail.com>
2023-12-14 07:13:06 +0530

    Updated :sparkles: question #2482

--- src/Arrays/Medium/DiffBetween1sN0sInRowNCol_2482.java
+++ src/Arrays/Medium/DiffBetween1sN0sInRowNCol_2482.java
@@ -2,27 +2,27 @@ package Arrays.Medium;
 
 // 2482. Difference Between Ones and Zeros in Row and Column
 // https://leetcode.com/problems/difference-between-ones-and-zeros-in-row-and-column/
-// Runtime 5 ms Beats 96.80%
-// Memory 80.2 MB Beats 68.40%
-// Mar 28, 2023
+// Runtime 4 ms Beats 98.71%
+// Memory 73.34 MB Beats 42.40%
+// Dec 14, 2023
 
 public class DiffBetween1sN0sInRowNCol_2482 {
     public int[][] onesMinusZeros(int[][] grid) {
-        int m=grid.length,n=grid[0].length;
-        int[] rows=new int[m];
-        int[] cols=new int[n];
+        int m = grid.length, n = grid[0].length;
+        int[] rows = new int[m];
+        int[] cols = new int[n];
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                rows[i]+=grid[i][j];
-                cols[j]+=grid[i][j];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                rows[i] += grid[i][j];
+                cols[j] += grid[i][j];
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                grid[i][j]=2*rows[i]+2*cols[j]-m-n;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = 2 * rows[i] + 2 * cols[j] - m - n;
             }
-        }        
+        }
         return grid;
     }
 }
