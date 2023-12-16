7bc30f95535e7320dfda95bfb9c38acf2405b002
Radheshyam Soni <radheshyambsoni@gmail.com>
2023-12-16 08:02:05 +0530

    Updated :sparkles: question #242

--- src/Strings/Easy/ValidAnagram_242.java
+++ src/Strings/Easy/ValidAnagram_242.java
@@ -1,24 +1,25 @@
 package Strings.Easy;
 
+// 242. Valid Anagram
 // https://leetcode.com/problems/valid-anagram/
 // Runtime 1 ms Beats 100%
 // Memory 42.6 MB Beats 83.3%
 // December 29, 2022
 
-public class ValidAnagram {
+public class ValidAnagram_242 {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()){
+        if (s.length() != t.length()) {
             return false;
         }
-        int[] freq=new int[26];
-        for(char c:s.toCharArray()){
-            freq[c-'a']++;
+        int[] freq = new int[26];
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
         }
-        for(char c:t.toCharArray()){
-            freq[c-'a']--;
+        for (char c : t.toCharArray()) {
+            freq[c - 'a']--;
         }
-        for(int i:freq){
-            if(i!=0){
+        for (int i : freq) {
+            if (i != 0) {
                 return false;
             }
         }
