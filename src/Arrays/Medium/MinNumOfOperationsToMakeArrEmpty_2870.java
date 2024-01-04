b12bd99ed33a76f76cb54ee2d7e720caf449b6f7
Radheshyam Soni <radheshyambsoni@gmail.com>
2024-01-04 18:23:30 +0530

    Added :sparkles: question #2870

---
+++ src/Arrays/Medium/MinNumOfOperationsToMakeArrEmpty_2870.java
@@ -0,0 +1,32 @@
+package Arrays.Medium;
+
+import java.util.HashMap;
+
+// 2870. Minimum Number of Operations to Make Array Empty
+// https://leetcode.com/problems/minimum-number-of-operations-to-make-array-empty/
+// Runtime 19 ms Beats 73.58%
+// Memory 63.38 MB Beats 6.04%
+// Dec 04, 2023
+
+public class MinNumOfOperationsToMakeArrEmpty_2870 {
+    public int minOperations(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int cnt = 0;
+        for (int f : map.values()) {
+            if (f == 1) {
+                return -1;
+            } else {
+                cnt += f / 3;
+                if (f % 3 != 0) {
+                    cnt++;
+                }
+            }
+        }
+
+        return cnt;
+    }
+}
