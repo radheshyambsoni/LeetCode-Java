5976bafcbbd6e3d0acacb3bb3d71831d7155d311
Radheshyam Soni <geekyradhe11@gmail.com>
2024-01-12 22:35:08 +0530

    Updated :sparkles: question #1704

---
+++ src/Strings/Easy/DetermineIfStringHalvesAreAlike_1704.java
@@ -0,0 +1,77 @@
+package Strings.Easy;
+
+// 1704. Determine if String Halves Are Alike
+// https://leetcode.com/problems/determine-if-string-halves-are-alike/
+// Runtime 2 ms Beats 95.53%
+// Memory 41.82 MB Beats 24.13%
+// Jan 12, 2024
+
+public class DetermineIfStringHalvesAreAlike_1704 {
+    // Runtime 1 ms Beats 100%
+    // Memory 40.7 MB Beats 86.12%
+    public static boolean isVowel(char ch) {
+        switch (ch) {
+            case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U':
+                return true;
+        }
+        return false;
+    }
+
+    public boolean halvesAreAlike(String s) {
+        char[] str = s.toCharArray();
+        int count = 0, i = 0, j = str.length - 1;
+        while (i < j) {
+            if (isVowel(str[i++])) {
+                count++;
+            }
+            if (isVowel(str[j--])) {
+                count--;
+            }
+        }
+
+        return count == 0;
+    }
+
+    // public static boolean isVowel(char ch){
+    // switch(ch){
+    // case 'a':return true;
+    // case 'e':return true;
+    // case 'i':return true;
+    // case 'o':return true;
+    // case 'u':return true;
+    // case 'A':return true;
+    // case 'E':return true;
+    // case 'I':return true;
+    // case 'O':return true;
+    // case 'U':return true;
+    // }
+    // return false;
+    // }
+    // public static boolean halvesAreAlike(String s) {
+    // // int count=0,i=0,j=s.length()-1;
+    // // String vowels="aeiouAEIOU";
+    // // while(i<j){
+    // // if(vowels.indexOf(s.charAt(i++))!=-1){
+    // // count++;
+    // // }
+    // // if(vowels.indexOf(s.charAt(j--))!=-1){
+    // // count--;
+    // // }
+    // // }
+    // // return count==0;
+
+    // // Runtime 2 ms Beats 99.33%
+    // // Memory 40.1 MB Beats 99.87%
+    // int count1=0,count2=0,i=0,j=s.length()-1;
+    // while(i<j){
+    // if(isVowel(s.charAt(i++))){
+    // count1++;
+    // }
+    // if(isVowel(s.charAt(j--))){
+    // count2++;
+    // }
+    // }
+
+    // return count1==count2;
+    // }
+}
